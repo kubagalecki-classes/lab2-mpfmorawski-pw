1937d4c3f8053d81f0620bd902084bc4d7f6c9c9
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-11-11 18:45:01 +0000

    Lab 2 - Zadanie 4.

--- lab2.cpp
+++ lab2.cpp
@@ -28,15 +28,36 @@ public:
         delete[] vector;
     }
 
+    void print();
+
 private:
     int dlugosc;
 };
+
+void Wektor::print()
+{
+    std::cout << "[";
+    for (int i = 0; i < (dlugosc - 1); i++)
+        std::cout << vector[i] << ", ";
+    if (dlugosc > 0)
+        std::cout << vector[dlugosc - 1];
+    std::cout << "]" << std::endl;
+}
 
 int main()
 {
     Wektor Vector1{2};
     Wektor Vector2{};
-    std::cout << Vector1.getDlugosc() << std::endl;
-    std::cout << Vector2.getDlugosc() << std::endl;
-    std::cout << "Ostatnia linia kodu w main.\n";
+    Vector1.print();
+    Vector2.print();
 }
+
+/*
+Po wykonaniu otrzymano:
+  Konstruktor parametryczny wektora o dlugosci 2.
+  Konstruktor domyslny wektora.
+  [0, 0]
+  []
+  Destruktor wektora.
+  Destruktor wektora.
+*/
