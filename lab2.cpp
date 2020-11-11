c227ca17400c0085e9ce052c44e5ddc860172ea5
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-11-11 21:55:19 +0000

    Zal 1

--- lab2.cpp
+++ lab2.cpp
@@ -1,172 +1,29 @@
 #include <iostream>
 
-class Wektor
+// Przeciążenie nr 1, zwykła referencja
+void print_int(const int& i)
 {
-
-public:
-    Wektor() : dlugosc{0}, pojemnosc{0}
-    {
-        std::cout << "Konstruktor domyslny wektora.\n";
-        vector = new double[0];
-    }
-
-    Wektor(int vec_n) : dlugosc{vec_n}, pojemnosc{vec_n}
-    {
-        std::cout << "Konstruktor parametryczny wektora o dlugosci " << dlugosc << ".\n";
-        vector = new double[dlugosc];
-        for (int i = 0; i < dlugosc; i++)
-            vector[i] = 0.;
-    }
-
-    Wektor(const Wektor& W)
-    {
-        std::cout << "Konstruktor kopiujacy wektora.\n";
-        double* temp;
-        temp = new double[W.dlugosc];
-        for (int i = 0; i < W.dlugosc; i++)
-            temp[i] = W.vector[i];
-
-        vector       = temp;
-        double temp0 = temp[0];
-        delete[] temp;
-        vector[0] = temp0;
-
-        dlugosc   = W.dlugosc;
-        pojemnosc = W.pojemnosc;
-    }
-
-    Wektor& operator=(const Wektor& W)
-    {
-        std::cout << "Kopiujacy operator przypisania wektora.\n";
-        if (this != &W) {
-            double* temp;
-            temp = new double[W.dlugosc];
-            for (int i = 0; i < W.dlugosc; i++)
-                temp[i] = W.vector[i];
-
-            vector       = temp;
-            double temp0 = temp[0];
-            delete[] temp;
-            vector[0] = temp0;
-
-            dlugosc   = W.dlugosc;
-            pojemnosc = W.pojemnosc;
-        }
-        return *this;
-    }
-
-    ~Wektor()
-    {
-        std::cout << "Destruktor wektora.\n";
-        delete[] vector;
-    }
-
-    double& operator[](int);
-
-    int getDlugosc() { return dlugosc; }
-    int getPojemnosc() { return pojemnosc; }
-
-    void print();
-    void zmienDlugosc(int);
-
-private:
-    double* vector;
-    int     dlugosc;
-    int     pojemnosc;
-};
-
-double& Wektor::operator[](int i)
-{
-    if (i >= dlugosc)
-        zmienDlugosc(i + 1);
-
-    return vector[i];
+    std::cout << "Ref: " << i << '\n';
 }
 
-void Wektor::print()
+// Przeciążenie nr 2, RVR
+/*
+void print_int(int&& i)
 {
-    std::cout << "[";
-    for (int i = 0; i < (dlugosc - 1); i++)
-        std::cout << vector[i] << ", ";
-    if (dlugosc > 0)
-        std::cout << vector[dlugosc - 1];
-    std::cout << "]" << std::endl;
+    std::cout << "RVR: " << i << '\n';
 }
+*/
 
-void Wektor::zmienDlugosc(int newLength)
+int getInt()
 {
-    if (newLength <= pojemnosc) {
-        if (newLength < dlugosc) {
-            for (int i = newLength; i < dlugosc; i++)
-                vector[i] = 0.;
-        }
-        dlugosc = newLength;
-    }
-    else {
-        double* newvector = new double[newLength];
-
-        for (int i = 0; i < pojemnosc; i++)
-            newvector[i] = vector[i];
-        delete[] vector;
-        for (int i = pojemnosc; i < newLength; i++)
-            newvector[i] = 0.;
-
-        vector      = newvector;
-        double temp = vector[0];
-        delete[] newvector; // UWAGA: po tej linijce również vector[0] = 0
-        vector[0] = temp;
-        dlugosc   = newLength;
-        pojemnosc = newLength;
-    }
+    return 42;
 }
 
 int main()
 {
-    Wektor Vector1{3};
-    for (double i = 0.; i < Vector1.getDlugosc(); i++)
-        Vector1[i] = i + 1;
-    Vector1.print();
+    int liczba = 314159; // liczba to lvalue
 
-    Wektor Vector2{};
-    Vector2.print();
-
-    Vector2 = Vector1 = Vector1;
-    Vector2.print();
-
-    std::cout << std::endl;
-
-    Vector2[1] = 5.;
-    Vector1.print();
-    Vector2.print();
-
-    std::cout << std::endl;
-
-    Vector1[2] = 7.;
-    Vector1.print();
-    Vector2.print();
-
-    std::cout << std::endl;
-
-    Vector1[5] = 6.;
-    Vector1.print();
-    Vector2.print();
+    print_int(liczba);   // przeciążenie 1
+    print_int(getInt()); // przeciążenie 2
+    print_int(13);       // przeciążenie 2, bo '13' to rvalue
 }
-
-/*
-Po wykonaniu otrzymano:
-  Konstruktor parametryczny wektora o dlugosci 3.
-  [1, 2, 3]
-  Konstruktor kopiujacy wektora.
-  [1, 2, 3]
-
-  [1, 2, 3]
-  [1, 5, 3]
-
-  [1, 2, 7]
-  [1, 5, 3]
-
-  [1, 2, 7, 0, 0, 6]
-  [1, 5, 3]
-  Destruktor wektora.
-  Destruktor wektora.
-*/
--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -4,5 +4,60 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+
+public:
+
+  ResourceManager()
+  {
+    std::cout << "Konstruktor domyslny.\n";
+    resource = new Resource;
+  }
+
+  ResourceManager(const ResourceManager& RM)
+  {
+    std::cout << "Konstruktor kopiujacy.\n";
+    resource = RM.resource;
+  }
+
+  ResourceManager& operator=(const ResourceManager& RM)
+  {
+    std::cout << "Kopiujacy operator przypisania.\n";
+    if(this != &RM)
+    {
+      resource = RM.resource;
+    }
+    return *this;
+  }
+
+  ResourceManager(ResourceManager&& RM)
+  {
+    std::cout << "Konstruktor przenoszacy\n";
+    resource = RM.resource;
+    RM.resource = NULL;
+  }
+
+  ResourceManager& operator=(ResourceManager&& RM)
+  {
+    std::cout << "Przenoszacy operator przypisania.\n";
+    if(this != &RM)
+    {
+      resource = RM.resource;
+      RM.resource = nullptr;
+    }
+    return *this;
+  }
+
+  double get()
+  {
+    return resource.get();
+  }
+
+  ~ResourceManager()
+  {
+    std::cout << "Destruktor.\n";
+    delete resource;
+  }
+
+private:
+  Resource *resource = nullptr;
 };
